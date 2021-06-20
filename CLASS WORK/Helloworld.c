af51993ae15e6b17c2b757d9666c52d871e4a907
SEETHA DINESH <82213353+seetha-142@users.noreply.github.com>
2021-06-20 09:54:54 +0530

    Add files via upload

---
+++ CLASS WORK/Helloworld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    printf("\n\n\n");
+    printf("             H       H  EEEEEE  L       L        OOOO     W              W    OOOO     RRRRRRR   L       DDDDDD       \n");
+    printf("             H       H  E       L       L      OO    OO    W            W   OO    OO   R      R  L       D      D     \n");
+    printf("             H HHHHH H  EEEEEE  L       L      OO    OO     W    WW    W    OO    OO   R RRRRRR  L       D       D    \n");
+    printf("             H       H  E       L       L      OO    00      W  W  W  W     OO    OO   R R       L       D      D     \n");
+    printf("             H       H  EEEEEE  LLLLLL  LLLLLL   OOOO         W      W        OOOO     R   RR    LLLLLL  DDDDDD       \n\n\n");
+}
